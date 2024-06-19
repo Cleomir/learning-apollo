1383893da1a076ea612bb2cb1d4f3a6841c4b503
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-06-19 20:23:41 -0300

    Add resolvers for querying individual games, authors, and reviews

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,7 +3,16 @@ import { authors, games, reviews } from "../db";
 export const resolvers = {
   Query: {
     games: () => games,
+    game: (_: any, args: any) => {
+      return games.find((game) => game.id === args.id);
+    },
     authors: () => authors,
+    author: (_: any, args: any) => {
+      return authors.find((author) => author.id === args.id);
+    },
     reviews: () => reviews,
+    review: (_: any, args: any) => {
+      return reviews.find((review) => review.id === args.id);
+    },
   },
 };
